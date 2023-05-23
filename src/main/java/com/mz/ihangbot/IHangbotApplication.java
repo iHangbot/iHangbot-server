30536409ad55684ad7fce8978a39e4fc06c26aab
Jimin0304 <pjws0304@naver.com>
2023-05-23 21:03:19 +0900

    initial commmit

---
+++ src/main/java/com/mz/ihangbot/IHangbotApplication.java
@@ -0,0 +1,13 @@
+package com.mz.ihangbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IHangbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IHangbotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mz/ihangbot/IHangbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mz.ihangbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IHangbotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
