7030b4f42cf49f6a91a01b3334c4d2c62bf82686
Jimin0304 <pjws0304@naver.com>
2023-05-25 19:45:42 +0900

    chore(errorCode): 회원가입 시 확인 비밀번호가 다를 때 에러메세지 추가

--- src/main/java/com/mz/ihangbot/common/exception/ErrorCode.java
+++ src/main/java/com/mz/ihangbot/common/exception/ErrorCode.java
@@ -12,6 +12,7 @@ public enum ErrorCode {
     ENTITY_NOT_FOUND(400, "C_005", "지정한 Entity를 찾을 수 없습니다."),
 
     MEMBER_NOT_FOUND(400, "ME_001", "사용자를 찾을 수 없습니다."),
+    WRONG_PASSWORD(400, "ME_002","비밀번호가 틀립니다."),
 
     DUPLICATED_EMAIL(400, "AU_002", "이미 존재하는 E-mail입니다."),
     UNAUTHORIZED_REDIRECT_URI(400, "AU_003", "인증되지 않은 REDIRECT_URI입니다."),
