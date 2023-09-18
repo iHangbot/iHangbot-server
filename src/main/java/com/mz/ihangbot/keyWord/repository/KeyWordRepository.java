09b1324c54d319f5bff04f7cb140cbaf1358d224
Jimin0304 <pjws0304@naver.com>
2023-09-18 22:09:20 +0900

    fix: 제안 데이터 requestDTO 단어 상위 5개만 전송으로 수정

--- src/main/java/com/mz/ihangbot/keyWord/repository/KeyWordRepository.java
+++ src/main/java/com/mz/ihangbot/keyWord/repository/KeyWordRepository.java
@@ -16,7 +16,7 @@ public interface KeyWordRepository extends JpaRepository<KeyWord, Long>  {
     @Query(value = "select new com.mz.ihangbot.report.dto.KeyWordReportResponseDTO(result.kw, result.cnt) from (select k.keyword as kw, SUM(k.count) as cnt from KeyWord k where :username = k.member.username and :aWeekAgo <= k.date group by k.keyword) as result order by result.cnt DESC limit 5")
     List<KeyWordReportResponseDTO> getKeyWords(@Param("username") String username, @Param("aWeekAgo") Date aWeekAgo);
 
-    @Query(value = "select k from KeyWord k where :username = k.member.username and k.date >= :aWeekAgo")
+    @Query(value = "select k from KeyWord k where :username = k.member.username and k.date >= :aWeekAgo order by k.count DESC limit 5")
     List<KeyWord> findAllByUsernameAndDate(@Param("username") String username, @Param("aWeekAgo") Date aWeekAgo);
 
     @Query(value = "select k from KeyWord k where :username = k.member.username and k.date >= :aWeekAgo and k.keyword = :keyword")
